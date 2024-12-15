21a6d24a12e4abab098e2f0c0f22254f5b0c7a44
chasethesky79 <seshadri.bharath@yahoo.com>
2024-12-15 13:03:57 -0600

    Implemented styling on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import "./Header.css"
+import { Link as RouterLink } from 'react-router-dom'
 
 export default function Header({ logo }) {
     return (
         <header className="App-header">
           <h1>My Portfolio</h1>
+          <nav>
+            <RouterLink to="/" className='App-link'>
+                About me
+            </RouterLink>
+            <RouterLink to="/projects" className='App-link'>
+                Projects
+            </RouterLink>
+          </nav>
         </header>
     )
 }
